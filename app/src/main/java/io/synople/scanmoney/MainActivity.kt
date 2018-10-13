b989c5aa6924d358b28e24e2ee8b7590fc898997
Jason Tu <title.kubo@gmail.com>
2018-10-12 22:09:15 -0400

    ARCore now working via Sceneform UX

--- app/src/main/java/io/synople/scanmoney/MainActivity.kt
+++ app/src/main/java/io/synople/scanmoney/MainActivity.kt
@@ -1,12 +1,15 @@
 package io.synople.scanmoney
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.app.FragmentActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : FragmentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.frameLayout, ARFragment.newInstance()).commit()
     }
 }
